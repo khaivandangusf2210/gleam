7922ab9d1139757d74390b720e80014f04b05e10
paulocilasjr <pj_ibm@hotmail.com>
2026-01-11 10:23:53 -0500

    fix: import order

--- tools/multimodallearner/split_logic.py
+++ tools/multimodallearner/split_logic.py
@@ -1,8 +1,8 @@
 import logging
 from typing import List, Optional
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 logger = logging.getLogger(__name__)
